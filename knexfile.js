c40e789c23ac6a2337460ba7b1e1672d82c2bbb8
Bernice Tan <bernice.tann@gmail.com>
2017-05-24 00:03:29 +0000

    Migrated with knex

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+const settings = require('./settings');
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://development:development@localhost:5432/vagrant?ssl=true'
+  },
+
+};
+
